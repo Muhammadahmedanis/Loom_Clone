795ee3509f139f1d4997097c89dde79647fd7167
Muhammad Ahmed <153613965+Muhammadahmedanis@users.noreply.github.com>
2025-06-20 18:54:25 +0500

    ..

--- app/(root)/(home)/meeting/[id]/page.tsx
+++ app/(root)/(home)/meeting/[id]/page.tsx
@@ -1,35 +1,35 @@
+// page.tsx
 'use client'
 
 import Loader from "@/components/Loader";
 import MeetingRoom from "@/components/MeetingRoom";
 import MeetingSetup from "@/components/MeetingSetup";
 import { useGetCallById } from "@/hooks/useGetCallById";
-import { useUser } from "@clerk/nextjs"
+import { useUser } from "@clerk/nextjs";
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
 import { useState } from "react";
 
-interface MeetingPageProps {
-  params: { id: string }
-}
 
-export default function Meeting({ params }: MeetingPageProps) {
+export default function Meeting({ params }: { params: { id: string } }) {
     const { isLoaded } = useUser(); 
-    const[isSetupComplete, setIsSetupComplete] = useState(false);
+    const [isSetupComplete, setIsSetupComplete] = useState(false);
     const { call, isCallLoading } = useGetCallById(params?.id);
 
-    if(!isLoaded || isCallLoading) return <Loader />
+    if (!isLoaded || isCallLoading) return <Loader />;
 
-  return (
-   <div className="h-screen w-full">
-    <StreamCall call={call}>
-        <StreamTheme>
-            {
-                !isSetupComplete ?
-                ( <MeetingSetup setIsSetupComplete={setIsSetupComplete} /> ): 
-                ( <MeetingRoom /> ) 
-            }
-        </StreamTheme>
-    </StreamCall>
-   </div> 
-  )
+    return (
+        <div className="h-screen w-full">
+            <StreamCall call={call}>
+                <StreamTheme>
+                    {
+                        !isSetupComplete ? (
+                            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+                        ) : (
+                            <MeetingRoom />
+                        )
+                    }
+                </StreamTheme>
+            </StreamCall>
+        </div>
+    );
 }
